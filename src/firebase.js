3784450079a5aafac7c66b04169c65625bf612b6
alina-asievskaya <1227112@mtp.by>
2025-07-16 02:04:23 +0300

    first

---
+++ src/firebase.js
@@ -0,0 +1,15 @@
+import {initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCJBJaJ8SZ6dNO-CpSK0wJ-TaCzVZFe7jU",
+  authDomain: "food-delivery-98920.firebaseapp.com",
+  projectId: "food-delivery-98920",
+  storageBucket: "food-delivery-98920.firebasestorage.app",
+  messagingSenderId: "642358031620",
+  appId: "1:642358031620:web:043a1c0a5920338bc30c51",
+  measurementId: "G-8HF7E2H9MQ"
+};
+
+const app = initializeApp(firebaseConfig);
+
+export default app;
